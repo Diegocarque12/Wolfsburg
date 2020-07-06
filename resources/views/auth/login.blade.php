1673b75bfd0c28b81efeec93616af6211ac6e284
Diego Carvajal <diegocarque@hotmail.com>
2020-07-06 14:25:48 -0600

    Agregue los app que contenia js y css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
-
+@section('imports')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+@endsection
 @section('content')
 
 <section>
@@ -57,6 +62,11 @@
                 </div>
             </div>
         </form>
+    </div>
+    
+
+    <div class="registro">
+
     </div>
 </section>
 
