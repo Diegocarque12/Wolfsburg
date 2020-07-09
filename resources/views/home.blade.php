cca2e96660a0ce5235233500a4d5ce64fbccda66
Diego Carvajal <diegocarque@hotmail.com>
2020-07-09 01:33:15 -0600

    Agregue los assets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
  <section class="section--main">
-      <img src="../src/home-firstCopy.jpg" class="section__img" alt="">
+      <img src="{{asset('../src/home-firstCopy.jpg')}}" class="section__img" alt="">
       <div class="section__text">
         <h2 class="section__text--h2">Menú</h2>
         <h3 class="section__text--h3">Gourmet</h3>
@@ -17,9 +17,9 @@
   <section class="section--second">
     <p class="section__text--p">Estas son algunas imágenes de nuestros platillos, podrían ser entregados y servidos hasta la puerta de tu casa.</p>
     <div class="section__img ">
-      <img src="../src/platillo1.jpg" class="section__img--top-left" alt="">
-      <img src="../src/platillo3.jpg" class=" section__img--right mx-auto d-block" alt="">
-      <img src="../src/platillo2.jpg" class="section__img--bott-left" alt="">
+      <img src="{{asset('../src/platillo1.jpg')}}" class="section__img--top-left" alt="">
+      <img src="{{asset('../src/platillo3.jpg')}}" class=" section__img--right mx-auto d-block" alt="">
+      <img src="{{asset('../src/platillo2.jpg')}}" class="section__img--bott-left" alt="">
     </div>
     
     <div class="section--delivery-info">
@@ -30,12 +30,12 @@
   <section class="section--sponsor">
     <p>Algunos de nuestros patrocinadores</p>
     <div class="section--sponsor__items text-center ">
-      <img class="section__img" src="../src/p-joseph.png" alt="">
-      <img class="section__img" src="../src/p-beer.png" alt="">
-      <img class="section__img" src="../src/p-jw.png" alt="">
-      <img class="section__img" src="../src/p-uber.png" alt="">
-      <img class="section__img" src="../src/p-gs.png" alt="">
-      <img class="section__img" src="../src/p-thepearl.png" alt="">
+      <img class="section__img" src="{{asset('../src/p-joseph.png')}}" alt="">
+      <img class="section__img" src="{{asset('../src/p-beer.png')}}" alt="">
+      <img class="section__img" src="{{asset('../src/p-jw.png')}}" alt="">
+      <img class="section__img" src="{{asset('../src/p-uber.png')}}" alt="">
+      <img class="section__img" src="{{asset('../src/p-gs.png')}}" alt="">
+      <img class="section__img" src="{{asset('../src/p-thepearl.png')}}" alt="">
     </div>
   </section>
 
