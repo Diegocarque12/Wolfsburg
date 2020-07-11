f3a77110cc71877e93726aaa8c06ffa6473ce42f
Diego Carvajal <diegocarque@hotmail.com>
2020-07-11 00:44:32 -0600

    contact us text

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -5,12 +5,21 @@
 <section class="about-us">
         <h1 class="about-us__title"> <span class="about-us__mark">Nuestro </span>   Equipo</h1>
         <div class="about-us__container1 ">
-            <img class="about-us__container1 about-us__container1--img1" src="../src/chef1.jpg" alt="">
-            <p class="about-us__container1 about-us__container1--paragrah1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores maxime, qui cum totam accusantium eum voluptates repudiandae quasi recusandae eligendi alias repellat, impedit voluptate ipsam commodi. Expedita earum pariatur a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, libero numquam voluptas vitae aliquid sit. Nostrum suscipit consequuntur obcaecati minus, facilis ad maiores ullam sint quo ratione, nobis ducimus quidem..</p>
+            <img class="about-us__container1 about-us__container1--img1" src="{{assets('../src/chef1.jpg')}}" alt="">
+            <p class="about-us__container1 about-us__container1--paragrah1">
+            Nuestro restaurante es considerado como el mejor de la ciudad de Wolfsburgo, ciudad al norte de Alemania,
+             donde se encuentra situado nuestro restaurante.
+             Nuestra mayor satisfacción es ver una sonrisa en el rostro
+             de nuestros comensales. Y brindarles una experiencia gastronómica propia de nuestro equipo de chefs</p>
         </div>
         <div class="about-us__container2">
-            <p class="about-us__container2 about-us__container2--paragrah2" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta sunt dolor quidem culpa dolores nisi, in ad accusantium voluptatem nam. Doloremque facere eligendi omnis vitae. Veritatis nisi ea quisquam dolor! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolores error itaque, autem, mollitia dolore adipisci non vel illum ut corporis voluptates? Incidunt fugiat voluptates sit eos, omnis id consequatur?</p>
-            <img class="about-us__container2 about-us__container2--img2"  src="../src/chef2.jpg" alt="">
+            <p class="about-us__container2 about-us__container2--paragrah2" >
+            Estas imagenes pertenecen al chef líder de la cocina. El es el famoso Harald Wohlfahrt,
+            que mantiene el orden dentro de la cocina junto con sus otros ayudantes, también chefs que acompañan su labor.
+            En general, el ambiente de la cocina es cooperativo y busca que todos los integrantes 
+            mantengan la misma visión, dar lo mejor de cada uno, como si fuera el último platillo que fueran a preparar.
+            </p>
+            <img class="about-us__container2 about-us__container2--img2"  src="{{assets('../src/chef2.jpg')}}" alt="">
         </div>
     
 </section>
